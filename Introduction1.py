9b4e3e5538245834b5c14eabff9a5bfca21a2528
username22194 <147832741+username22194@users.noreply.github.com>
2024-01-05 13:30:26 +0100

    Create Introduction1.py

---
+++ Introduction1.py
@@ -0,0 +1,15 @@
+import time
+Introduction = [
+    "L'histoire prend place au 9e siècle après J.C.. ", 
+    "Une rumeur court dans le village de Ravensbrook en Norvège : ", 
+    "un roi puissant aurait régné sur la contrée il y 200 ans. ", 
+    "Un joyau d'une valeur inestimable aurait été découvert dans la montagne du nord : ", 
+    "une montagne où résident de nombreuses créatures terrifiantes. ", 
+    "Ce roi aurait donc trouvé ce joyau et l'aurait mis sur sa couronne. ", 
+    "Seulement, les monstres de la montagne sont puissants. ", 
+    "Son royaume fût assiégé par ces créatures, le poussant à l'exil. ", 
+    "Le roi serait parti à la hâte, faisant tomber son joyau dans la forêt de la Montagne Enchantée. "
+    ] #9 objets
+for phrase in Introduction:
+    print(phrase)
+    time.sleep(2) #attente de 2 secondes avant la prochaine ligne
